c9b57c7b49c5ed94e4022a71ac567d0e77f1e1d4
Sebastien <Sebastien@Sebastien-PC>
2017-11-19 20:25:39 -0500

    added 'receiveData' method added the reception of a Frame and the writing of ACK frame in 'handleClientSocket' method

--- src/receiver/ReceiverThread.java
+++ src/receiver/ReceiverThread.java
@@ -1,8 +1,17 @@
 package receiver;
+import frames.AckFrame;
+import frames.ConversionUtils;
+import frames.Frame;
+import frames.FrameFactory;
+import frames.MalformedFrameException;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import utils.NetworkAbstraction;
 
 public class ReceiverThread extends Thread {
 	
@@ -27,15 +36,31 @@ public class ReceiverThread extends Thread {
 	
 	// TODO bulk of the client side connection handling should be done here
 	private void handleClientSocket() throws IOException {
-		int nbBytesRead, nbBytesWritten;
-		OutputStream ostream = clientSocket.getOutputStream();
-		InputStream istream = clientSocket.getInputStream();
-		
-		// Beginning of connection, MUST be a C Frame
-		nbBytesRead = istream.read(buf);
-		
-		System.out.println("Sending to client on socket " + this.clientSocket);
-		ostream.write(("TEST Writing to socket" + clientSocket + "\n").getBytes());
-		clientSocket.close();
+            try {
+                int nbBytesRead, nbBytesWritten;
+                OutputStream ostream = clientSocket.getOutputStream();
+                InputStream istream = clientSocket.getInputStream();
+                // Beginning of connection, MUST be a C Frame
+                nbBytesRead = istream.read(buf);
+                
+                Frame f = receiveData(new DataInputStream(istream));
+                
+                System.out.println("Sending to client on socket " + this.clientSocket);
+                
+                //ostream.write(("TEST Writing to socket" + clientSocket + "\n").getBytes());
+                
+                // ACK:
+                ostream.write(new AckFrame(f.getNum()).getBytesWithCRC());
+                
+                clientSocket.close();
+            } catch (MalformedFrameException ex) {
+                Logger.getLogger(ReceiverThread.class.getName()).log(Level.SEVERE, null, ex);
+            }
 	}
+        
+        private Frame receiveData(DataInputStream dataInput) throws IOException, MalformedFrameException {
+            Frame frame = null;
+            frame = NetworkAbstraction.receiveFrame(dataInput);
+            return frame;
+        }
 }
