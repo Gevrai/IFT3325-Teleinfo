bf54b11d1731fe9cde7a1cb4004dc2306ddb154f
Sebastien <Sebastien@Sebastien-PC>
2017-11-19 20:27:30 -0500

    added 'sendData' and 'getter' methods

--- src/sender/Sender.java
+++ src/sender/Sender.java
@@ -1,5 +1,6 @@
 package sender;
 
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,17 +9,17 @@ import java.net.Socket;
 
 public class Sender {
 
-	private String 	machineName;
-	private int 	portNumber;
-	private String 	fileName;
-	private int 	goBackAmmount;
-    
-	public Sender(String machineName, String portNumber, String fileName, String goBackAmmount) {
+	String      machineName;
+	int         portNumber;
+	private String      fileName;
+	private int         goBackAmount;
+        
+	public Sender(String machineName, String portNumber, String fileName, String goBackAmount) {
 		// TODO vérifier la validité des informations données
 		this.machineName = machineName;
 		this.portNumber = Integer.parseUnsignedInt(portNumber);
 		this.fileName = fileName;
-		this.goBackAmmount = Integer.parseUnsignedInt(goBackAmmount);
+		this.goBackAmount = Integer.parseUnsignedInt(goBackAmount);
 	}
 	
 	// Just testing.....
@@ -26,9 +27,19 @@ public class Sender {
 		Socket socket = new Socket(this.machineName, this.portNumber);
 		OutputStream ostream = socket.getOutputStream();
 		InputStream istream = socket.getInputStream();
-
 	}
-
+        
+        public void sendData(Socket soc) throws IOException {
+            try{
+                DataOutputStream dataOutput = new DataOutputStream(soc.getOutputStream());
+                
+                // TODO: send data...
+            }
+            catch(IOException e){
+                System.out.println(e);
+            }
+        }
+        
 	public static void main(String[] args) {
 		// Sender <Nom_Machine> <Numero_Port> <Nom_fichier> <0>
 		if (args.length != 4) {
@@ -36,13 +47,20 @@ public class Sender {
 			System.out.println("Invalid arguments, should be of form : Sender <Nom_Machine> <Numero_Port> <Nom_fichier> <0>");
 			return;
 		}
-		
-		try {
-			Sender sender = new Sender(args[0], args[1], args[2], args[3]);
-			File fileToSend = new File(args[2]);
-			sender.sendFile(fileToSend);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+                
 	}
+        
+        // Getters
+        public String getMachineName(){
+            return this.machineName;
+        }
+        public int getPortNumber(){
+            return this.portNumber;
+        }
+        public String getFileName(){
+            return this.fileName;
+        }
+        public int getGoBackAmount(){
+            return this.goBackAmount;
+        }
 }
