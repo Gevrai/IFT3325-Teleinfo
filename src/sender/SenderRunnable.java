30eaf76854922da745ee97f796156fa3be1361b0
Sebastien <Sebastien@Sebastien-PC>
2017-11-23 17:40:41 -0500

    the class SenderRunnable implements the run() method

---
+++ src/sender/SenderRunnable.java
@@ -0,0 +1,30 @@
+package sender;
+
+
+import frames.CommsRunnable;
+import frames.ConnectionFrame;
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import network.NetworkAbstraction;
+
+/**
+ *
+ * @author Sebastien
+ */
+public class SenderRunnable extends CommsRunnable{
+    
+    ExecutorService executor;
+    byte connectionType;
+    ConnectionFrame cFrame;
+    NetworkAbstraction network;
+    
+    public void run(){
+        try {
+            network.sendFrame(cFrame);
+        } catch (IOException ex) {
+            Logger.getLogger(SenderRunnable.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
