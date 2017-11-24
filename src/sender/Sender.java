d7b08e83834650c2e7da6b177d94b63c5ae37fdc
Sebastien <Sebastien@Sebastien-PC>
2017-11-24 17:24:13 -0500

    added throws to 'main'

--- src/sender/Sender.java
+++ src/sender/Sender.java
@@ -5,23 +5,24 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.concurrent.ExecutionException;
 
 import sessions.Session;
 
 public abstract class Sender {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		// Sender <Nom_Machine> <Numero_Port> <Nom_fichier> <0>
 		if (args.length != 4) {
 			System.err.println("Invalid arguments, should be of form : Sender <Nom_Machine> <Numero_Port> <Nom_fichier> <0>");
 			return;
 		}
-		
+                
 		String machineName = args[0];
 		String portNumber = args[1];
 		String fileName = args[2];
 		String connectionType = args[3];
-
+                
 		try {
 			InputStream fileStream = new FileInputStream(new File(fileName));
 			// Connect the session
