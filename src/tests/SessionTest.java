735125f93c2929ba66340b2ae0f4371b230b6280
Gevrai J-Tremblay <gevrai@gmail.com>
2017-11-25 03:58:21 -0500

    JUnit test for frame connection

---
+++ src/tests/SessionTest.java
@@ -0,0 +1,39 @@
+package tests;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import frames.ConnectionFrame;
+import receiver.Receiver;
+import sessions.Session;
+
+class SessionTest {
+
+	@Test
+	void ConnectAndDisconnectTest() throws IOException {
+
+		double receiverErrorRatio = 0.5;
+
+		// Start a receiver in another thread
+		Receiver receiver = new Receiver(0, receiverErrorRatio);
+		Thread t = new Thread() {
+			@Override
+			public void run() { 
+				try { 
+					receiver.acceptConnections(); 
+				} catch (IOException e) { 
+					fail(); }}};
+		t.start();
+
+		// Try connecting and then unconnecting
+		Session session = Session.connect("localhost", receiver.getLocalPort(), ConnectionFrame.STOP_AND_WAIT);
+		assertTrue(session != null);
+		
+		session.close();
+	}
+
+}
