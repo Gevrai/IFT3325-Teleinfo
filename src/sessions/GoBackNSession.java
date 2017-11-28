d876a0836cefbe05871dfc04ebb5a468706ed649
Gevrai J-Tremblay <gevrai@gmail.com>
2017-11-27 21:19:47 -0500

    Small fix upon reaching end of istream, still getting NullPointerException

--- src/sessions/GoBackNSession.java
+++ src/sessions/GoBackNSession.java
@@ -21,30 +21,30 @@ public class GoBackNSession extends Session {
 	@Override
 	public boolean send(InputStream istream) throws IOException {
 		byte[] buf = new byte[Frame.MAX_DATA_SIZE];
+		int nbBytesRead;
 		NumWindow sendingWindow = new NumWindow(Frame.MAX_NUM, Frame.MAX_NUM - 1);
 		byte currentFrameNum = 0;
-		Frame receivedFrame;
+		Frame sentFrame, receivedFrame;
 		
 		// As long as there is still data to send
 		while (istream.available() > 0 || !sendingWindow.isEmpty()) {
 
-			Log.verbose("Sending frame " + currentFrameNum);
+			// Check if we need to send anything
+			while (sendingWindow.hasNextToSend())
+				network.sendFrame(sendingWindow.getNextToSendAndStamp());
 
 			// While there is place in window, place a frame and send it
-			while (sendingWindow.canPut(currentFrameNum)) {
-				int nbBytesRead = istream.read(buf);
-				sendingWindow.put(new InformationFrame(currentFrameNum, Arrays.copyOf(buf, nbBytesRead)));
-				currentFrameNum = (byte) ((currentFrameNum+1)%Frame.MAX_NUM);
+			while (sendingWindow.canPut(currentFrameNum) && istream.available() > 0) {
+				nbBytesRead = istream.read(buf);
+				sentFrame = new InformationFrame(currentFrameNum, Arrays.copyOf(buf, nbBytesRead));
+				sendingWindow.put(sentFrame);
 				network.sendFrame(sendingWindow.getNextToSendAndStamp());
+				currentFrameNum = (byte) ((currentFrameNum+1)%Frame.MAX_NUM);
 			}
 
 			// If first frame in window expired, reset there
 			if (sendingWindow.hasFirstExpired(TIMEOUT_TIME))
 				sendingWindow.goBackTo(sendingWindow.getCurrentFirstAck());
-			
-			// Check if we need to send anything
-			while (sendingWindow.hasNextToSend())
-				network.sendFrame(sendingWindow.getNextToSendAndStamp());
 			
 			// Process received frames
 			if (this.receptionQueue.isEmpty())
@@ -52,6 +52,7 @@ public class GoBackNSession extends Session {
 			while ((receivedFrame = this.receptionQueue.poll()) != null) {
 				switch (receivedFrame.getType()) {
 				case AckFrame.TYPE :
+					Log.println("Got ack frame for " + receivedFrame.getNum());
 					sendingWindow.ackUpTo(receivedFrame.getNum());
 					break;
 				case RejFrame.TYPE :
